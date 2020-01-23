a7ceaa1b9f4c119c347afa759217c032afbd694d
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-01-23 07:36:42 +0100

    Python only upto 16

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import legendre
 def main():
     f = lambda x: math.log(x + 2)
     actual_value = math.log(27) - 2
-    for n in [2, 4, 8, 16, 32]:
+    for n in [2, 4, 8, 16]:
         int_value = legendre.gauss_quadratur(n)(f)
         error = abs(int_value - actual_value)
         print(f"n = {n}, int = {int_value}, err={error}")
