a1c5e61e73a9a945841c7d7b911e38166147b175
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-01-25 12:14:41 +0100

    Rename legendre-functions and add plan to create macro that creates these functions for me

--- legendre.lisp
+++ legendre.lisp
@@ -3,7 +3,7 @@
 (ql:quickload :alexandria)
 (use-package :alexandria)
 
-(defun legendre (k)
+(defun legendre-0 (k)
   "Returns a funktion that evaluates the k'th Legendre Polynomial at x"
   (declare
    (optimize (speed 3))
@@ -15,16 +15,16 @@
      (lambda (x)
        (declare (double-float x))
        (iter
-        (for l from 2 to k)
-        (for x-2 previous x-1 initially 1)  ; value of (l-2)th LP(x)
-        (for x-1 previous x-0 initially x)  ; value of (l-1)th LP(x)
-        (for x-0 =                          ; value of lth LP(x)
-             (/ (- (* (1- (* 2 l)) x x-1)
-                   (* (1- l) x-2))
-                l))
-        (finally (return x-0)))))))
+         (for l from 2 to k)
+         (for x-2 previous x-1 initially 1)  ; value of (l-2)th LP(x)
+         (for x-1 previous x-0 initially x)  ; value of (l-1)th LP(x)
+         (for x-0 =                          ; value of lth LP(x)
+              (/ (- (* (1- (* 2 l)) x x-1)
+                    (* (1- l) x-2))
+                 l))
+         (finally (return x-0)))))))
 
-(defun legendre-1st-deriv (k)
+(defun legendre-1 (k)
   "Returns a function that evaluates the first derivative
 of the k'th Legendre Polynomial"
   (declare
@@ -37,26 +37,41 @@ of the k'th Legendre Polynomial"
      (lambda (x)
        (declare (double-float x))
        (iter
-        (for l from 2 to k)
-        (for x-1 = (funcall (legendre (1- l)) x))      ; value of (l-1)th LP(x)
-        (for x1-2 previous x1-1 initially 0)           ; value of (l-2)th LP'(x)
-        (for x1-1 previous x1-0 initially 1)           ; value of (l-1)th LP'(x)
-        (for x1-0 =                                    ; value of l th LP'(x)
-             (/ (- (* (1- (* 2 l)) (+ (* x x1-1) x-1))
-                   (* (1- l) x1-2))
-                l))
-        (finally (return x1-0)))))))
+         (for l from 2 to k)
+         (for x-1 = (funcall (legendre-0 (1- l)) x)) ; value of (l-1)th LP(x)
+         (for x1-2 previous x1-1 initially 0)      ; value of (l-2)th LP'(x)
+         (for x1-1 previous x1-0 initially 1)      ; value of (l-1)th LP'(x)
+         (for x1-0 =                               ; value of l th LP'(x)
+              (/ (- (* (1- (* 2 l)) (+ (* x x1-1) x-1))
+                    (* (1- l) x1-2))
+                 l))
+         (finally (return x1-0)))))))
+
+;; TODO
+;; There's an abstraction here for reducing the two (very) similar legendre functions, but I don't quite see it yet
+;; Maybe derive the legendre polynomials further and see if you find a pattern
+;; I'm assuming right now that each additional derivative adds one variable, aka one more line in the iter block
+;; see if that's correct, and if so, the complicated part is figuring out how to build a proper transform method for calculating the actual value
+;; each time you derive the expression, there's one term gets added to (+ (* x x1-1) x-1)
+;; (* x x-1) -> (+ (* x x1-1) x-1) -> (+ (* x x2-1) x1-1 x-1) ...
+;;
+;; => write a macro that (legendre n), n >= 0 that creates n+1 functions (legendre-0,..,legendre-n) that
+;; evaluate the polynomials using loops as above
+
+;; (defmacro legendre (k transform (&key case-0 case-1)))
 
 (defun fixed-point (f x0)
   (declare
    (double-float x0)
    (optimize (speed 3)))
-  (iter
-   (repeat 10000)
-   (for y0 previous yk initially x0)
-   (for yk = (funcall f y0))
-   (until (< (abs (- yk y0)) 1.0d-15))
-   (finally (return yk))))
+  (flet ((good-enough? (xk xk-1)
+           (< (abs (- xk xk-1)) 1.0d-15)))
+    (iter
+      (repeat 10000)
+      (for y0 previous yk initially x0)
+      (for yk = (funcall f y0))
+      (until (good-enough? yk y0))
+      (finally (return yk)))))
 
 (defun newton-method (f df x0)
   (flet ((newton-transform (g dg)
@@ -66,20 +81,20 @@ of the k'th Legendre Polynomial"
 
 (defun legendre-roots (n)
   (iter
-   (for k from 0 to n)
-   (for x0 = (cos (/ (* (+ (* 4 k) 3) pi)
-                     (+ (* 4 n) 6))))
-   (collect (newton-method (legendre (1+ n))
-                           (legendre-1st-deriv (1+ n))
-                           x0))))
+    (for k from 0 to n)
+    (for x0 = (cos (/ (* (+ (* 4 k) 3) pi)
+                      (+ (* 4 n) 6))))
+    (collect (newton-method (legendre-0 (1+ n))
+                            (legendre-1 (1+ n))
+                            x0))))
 
 (defun integration-weights (roots)
   (iter
-   (for xk in roots)
-   (with n = (1- (length roots)))
-   (collect (/ (* 2 (- 1 (expt xk 2)))
-               (* (expt (+ n 1) 2)
-                  (expt (funcall (legendre n) xk) 2))))))
+    (for xk in roots)
+    (with n = (1- (length roots)))
+    (collect (/ (* 2 (- 1 (expt xk 2)))
+                (* (expt (+ n 1) 2)
+                   (expt (funcall (legendre-0 n) xk) 2))))))
 
 (defmacro lookup (table n set-if-not-found)
   `(if (gethash ,n ,table)
@@ -93,31 +108,31 @@ of the k'th Legendre Polynomial"
            (weights (lookup cached-weights n (integration-weights roots))))
       (lambda (f)
         (iter
-         (for root in roots)
-         (for weight in weights)
-         (sum (* (funcall f root) weight)))))))
+          (for root in roots)
+          (for weight in weights)
+          (sum (* (funcall f root) weight)))))))
 
-(defmacro define-integrator (name fn int-value)
-  (alexandria:with-gensyms (gint)
+(defmacro define-integrator (name fn var int-value)
+  (alexandria:with-gensyms (gint gfn)
     `(defmacro ,name (supports)
        (let ((,gint ,int-value))
-         `(list ,@(mapcar
-                   (lambda (n)
-                     (let* ((int (funcall (gauss-quadratur n) #',fn))
-                            (err (abs (- int ,gint))))
-                       `(cons ,int ,err)))
-                   supports))))))
+         (flet ((,gfn (,var) ,fn))
+           `(list ,@(mapcar (lambda (n)
+                              (let* ((int (funcall (gauss-quadratur n) #',gfn))
+                                     (err (abs (- int ,gint))))
+                                `(cons ,int ,err)))
+                            supports)))))))
 
-(define-integrator herwig (lambda (x) (log (+ x 2.0d0))) (- (log 27.0d0) 2))
-(define-integrator joe (lambda (x) (/ 1 (+ 2 x))) (log 3.0d0))
+(define-integrator herwig (log (+ x 2)) x (- (log 27.0d0) 2))
+(define-integrator joe (/ 1 (+ 2 x)) x (log 3.0d0))
 
 (defmacro main (int n-values)
   (alexandria:with-gensyms (gn gresult gint gerr)
     `(iter
-      (with ,gresult = (time (,int ,n-values)))
-      (for ,gn in (list ,@n-values))
-      (for (,gint . ,gerr) in ,gresult)
-      (format t "n: ~2,,d     int: ~,16E     err: ~,16E~%" ,gn ,gint ,gerr))))
+       (with ,gresult = (time (,int ,n-values)))
+       (for ,gn in (list ,@n-values))
+       (for (,gint . ,gerr) in ,gresult)
+       (format t "n: ~2,,d     int: ~,16E     err: ~,16E~%" ,gn ,gint ,gerr))))
 
 (main herwig (2 4 8 16 32))
 (main joe (2 4 8 16 32))
